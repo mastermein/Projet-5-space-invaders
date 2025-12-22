7ae46d53d175ea5f424bec76c3936f96be199506
mastermein <kengnebrayan3@gmail.com>
2025-12-22 13:06:09 +0100

    Ajout de ImGui et SDL_Image + modifications du jeu

--- Files/Game.cpp
+++ Files/Game.cpp
@@ -1,1 +1,14 @@
+#include<SDL3/SDL.h>
+#include "Game.h"
+#include<iostream>
 
+SDL_Init(SDL_INIT_VIDEO);
+
+SDL_Window* window = SDL_CreateWindow( "Space invaders " , 800 , 600 , SDL_WINDOW_RESIZABLE);
+
+
+
+
+
+SDL_DestroyWindow(window);
+SDL_Quit();
